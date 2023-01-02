af3655798dbd79ef61360a5d38917d51d791aa0e
levworkshop <noreply@githubusers.com>
2023-01-02 16:30:04 +0200

    auth

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(headers);
-app.use('/', auth, indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cards', auth, cardsRouter);
 
